a4de9a1ae674a07e697d8f05e1b21c7aed759fa9
i-Karen <168977697+itznyperx1111@users.noreply.github.com>
2024-10-17 01:05:53 -0700

    Custom install.sh
    Can Install Panel V2.0.0 Or Older

--- install.sh
+++ install.sh
@@ -224,10 +224,10 @@ install_x-ui() {
     else
         tag_version=$1
         tag_version_numeric=${tag_version#v}
-        min_version="2.3.5"
+        min_version="2.0.0"
 
         if [[ "$(printf '%s\n' "$min_version" "$tag_version_numeric" | sort -V | head -n1)" != "$min_version" ]]; then
-            echo -e "${red}Please use a newer version (at least v2.3.5). Exiting installation.${plain}"
+            echo -e "${red}Please use a newer version (at least v2.0.0). Exiting installation.${plain}"
             exit 1
         fi
 
